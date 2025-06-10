2cdc8f93f335e6e2cdaa9c2d919e831e23719d59
Emerson Almeida <emersonalmeidax@gmail.com>
2025-06-10 10:08:55 -0300

    feat: group api list by team

--- src/components/apis/ApiList.tsx
+++ src/components/apis/ApiList.tsx
@@ -14,10 +14,28 @@ export default function ApiList({ apis }: ApiListProps) {
     );
   }
 
+  const apiByTeam = apis.reduce((acc, api) => {
+    if (!acc[api.team]) {
+      acc[api.team] = [];
+    }
+    acc[api.team].push(api);
+    return acc;
+  }, {} as Record<string, API[]>);
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {apis.map((api) => (
-        <ApiCard key={api.id} api={api} />
+    <div className="p-4">
+      {Object.entries(apiByTeam).map(([team, apis], i) => (
+        <div
+          key={team}
+          className={` ${i == 0 ? "" : "mt-8 pt-8 border-t border-gray-200"}`}
+        >
+          <h2 className="text-2xl font-bold mb-4">{team}</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {apis.map((api) => (
+              <ApiCard key={api.id} api={api} />
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   );
